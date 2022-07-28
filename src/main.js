c88a0758dd52fa4d97949caf133c82f8f87462d4
NicolayHeavyFog <nikolay_on_parseltongue@mail.ru>
2022-07-28 19:15:41 +0700

    downgrade to vue 2

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import { createApp } from 'vue';
+// import { createApp } from 'vue';
+import Vue from 'vue';
 import App from './App.vue';
 
 import { message, timeStamp, str } from './data';
 import alrt from './function';
 
-createApp(App).mount('#app');
+Vue.config.productionTip = false;
+
+// createApp(App).mount('#app');
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
 alrt(message, timeStamp, str);
